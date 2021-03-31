aefd3d32a1faa91b7289c553cc4a120920aa661b
Jessica Duell <duellrdh@gmail.com>
2021-03-30 22:13:24 -0400

    completed

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require("path");
+const bodyParser = require('body-parser');
 
 const server = express ();
 
+server.use(bodyParser.urlencoded({extended: false}))
+
 server.get('/', (req, res) => {
-    res.status(200).json('Hello World')
+   res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+server.post('/hello', (req, res) => {
+    console.log(req.body)
+    res.status(200).json(`Hello ${req.body.name}`)
 })
 
 const PORT = process.env.PORT || 8000;
